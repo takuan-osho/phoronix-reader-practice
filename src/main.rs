61a6eb478b7993641847e035a69e678ebbf4eb5f
SHIMIZU Taku <shimizu.taku@gmail.com>
2016-02-06 18:53:32 +0900

    HTMLの中からtitleだけ拾う機能を追加。

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,38 @@
 extern crate hyper;
 extern crate select;
 
+use select::document::Document;
+use select::predicate::{Class, Name};
+use select::node::Node;
+
+struct Article {
+    title: String,
+    // link: String,
+    // details: String,
+    // summary: String,
+}
+
+impl Article {
+    fn get_articles() -> Vec<Article> {
+        Document::from_str(open_testing())
+            .find(Name("article")).iter()
+            .map(|node| Article::new(&node))
+            .collect()
+    }
+    
+    fn new(node: &Node) -> Article {
+        let header = node.find(Name("a")).first().unwrap();
+        Article{ title: header.text() }
+    }
+}
+
 fn open_testing() -> &'static str {
     include_str!("phoronix.html")
 }
 
 fn main() {
-    let phoronix = open_testing();
-    println!("{}", phoronix);
+    let phoronix_articles = Article::get_articles();
+    for article in phoronix_articles {
+        println!("{}", article.title);
+    }
 }
