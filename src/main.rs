572f17f80ad0cac1507d9a16833fe62d2b0dbb1d
SHIMIZU Taku <shimizu.taku@gmail.com>
2016-02-11 20:37:24 +0900

    出力されるテキストが色付きになるようにする。

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 extern crate hyper;
 extern crate select;
+extern crate term;
 mod article;
 mod homepage;
 mod phoronix_cli;
 mod linesplit;
 
 fn main() {
-    phoronix_cli::print();
+    phoronix_cli::print_colored();
 }
--- src/phoronix_cli.rs
+++ src/phoronix_cli.rs
@@ -1,17 +1,28 @@
 use article::Article;
 use homepage;
 use linesplit;
+use term;
 
-pub fn print() {
+pub fn print_colored() {
     let phoronix_articles = Article::get_articles(homepage::offline());
+    let mut terminal = term::stdout().unwrap();
     for article in phoronix_articles.iter().rev() {
-        println!("Title:   {}", article.title);
-        println!("Link:    https://www.phoronix.com/{}", article.link);
-        println!("Details: {}", article.details);
-        println!("Summary:");
-        for line in linesplit::split_by_chars(&article.summary, 77) {
-            println!(" - {}", line);
+        println!("Title:   ");
+        terminal.fg(term::color::BRIGHT_GREEN).unwrap();
+        terminal.attr(term::Attr::Bold).unwrap();
+        println!("{}", article.title);
+        terminal.reset().unwrap();
+        println!("Link:    ");
+        terminal.fg(term::color::BRIGHT_CYAN).unwrap();
+        println!("https://www.phoronix.com/{}", article.link);
+        terminal.reset().unwrap();
+        println!("Details: {}\nSummary:", article.details);
+        for line in linesplit::split_by_chars(&article.summary, 77).iter() {
+            print!(" - ");
+            terminal.attr(term::Attr::Bold).unwrap();
+            println!("{}", line);
+            terminal.reset().unwrap();
         }
-        print!("\n");
+        println!("");
     }
 }
